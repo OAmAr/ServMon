e42c5d9a01483adb55ebd691d8b5e61e4a4c0d09
Omar <omar.amer@uconn.edu>
2017-10-12 03:37:37 +0000

    Small tweaks

--- geolocate.sh
+++ geolocate.sh
@@ -1,3 +1,13 @@
-cat /var/log/apache2/access.log | cut -d " " -f 1 |  { while read i; do echo "$i `geoiplookup $i`"; done } | sort | uniq -c | sort -k1,1nr -k6,6
+#!/bin/bash
+
+printf "____________________________________________________________________________\nApache Requests\n"
+cat /var/log/apache2/access.log | cut -d " " -f 1 | \
+{ while read i; do echo "$i `geoiplookup $i`"; done } | sort | uniq -c | sort -k1,1nr -k6,6 
+#| cut -d " " -f 7,8,12-
 printf "____________________________________________________________________________\nFailed root ssh logins\n"
-cat /var/log/auth.log | grep " exceeded for root" | cut -d : -f 2- | cut -d " " -f 12 | { while read i; do echo "$i `geoiplookup $i`"; done } | sort | uniq -c  | sort -k1,1nr -k6,6
+cat /var/log/auth.log | grep " exceeded for root" | cut -d : -f 2- | cut -d " " -f 12 |  \
+{ while read i; do echo "$i `geoiplookup $i`"; done } | sort | uniq -c  | sort -k1,1nr -k6,6 
+
+
+#| cut -d " " -f 7,8,12-
+
